105ca28bc07b224a18230728636ade574290bea9
srirammarimuthu <srirammarimuthu002@gmail.com>
2022-04-02 13:50:40 +0530

    Newly added file called new_cpp_file.cpp

---
+++ new_cpp_file.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+int main()
+{
+    string s="Sri.$J3.$S3.txt", f="Sri.089.002.txt";
+    if(s.find("$S") + 1)
+    {
+        int rem=((s.length() - s.find("$S")));
+        std::cout<<rem<<std::endl;
+    }
+}
